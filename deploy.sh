d139e1b096359e033a21e6e2c08a43ffc641faa9
gabrieladutra <gabriela_ddutra@outlook.com>
2025-08-04 10:49:11 -0300

    Adicionado configurações de responsividade

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ BUCKET='www.gabrieladutra.com'
 
 aws s3 rm s3://$BUCKET --recursive
 aws s3 cp dist s3://$BUCKET --recursive
-aws cloudfront create-invalidation --distribution-id E1XP1VN7BGCVA4 --paths '/*' no-cli-pager
+aws cloudfront create-invalidation --distribution-id E1XP1VN7BGCVA4 --paths '/*' --no-cli-pager
