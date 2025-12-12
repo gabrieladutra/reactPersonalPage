857c370a0fd3fc96f243ddacd3bd4f2de126d27f
gabrieladutra <gabriela_ddutra@outlook.com>
2025-12-12 16:09:20 -0300

    Melhorando deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-BUCKET='www.gabrieladutra.com'
 
+# Nome do bucket
+BUCKET='menu.gabrieladutra.com'
+
+# 1) GERAR O BUILD
+echo "Gerando build..."
+npm run build
+
+# 2) LIMPAR BUCKET
+echo "Limpando bucket..."
 aws s3 rm s3://$BUCKET --recursive
-aws s3 cp dist s3://$BUCKET --recursive
-aws cloudfront create-invalidation --distribution-id E1XP1VN7BGCVA4 --paths '/*' --no-cli-pager
+
+# 3) ENVIAR NOVO BUILD
+echo "Enviando novos arquivos..."
+aws s3 cp --acl public-read dist s3://$BUCKET --recursive
+
+# 4) INVALIDAR CLOUDFRONT
+echo "Invalidando CloudFront..."
+aws cloudfront create-invalidation \
+  --distribution-id E1XP1VN7BGCVA4 \
+  --paths '/*' \
+  --no-cli-pager
+
+echo "Deploy finalizado com sucesso!"
